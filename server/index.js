b2b5a30b937e0318c3b2e2bb43b3c325fba67f7a
avvelino <matheusavelino10@edu.unifor.br>
2022-12-13 10:31:32 -0300

    1.4v

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({
-  extended: true
+  extended: false
 }));
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -15,7 +15,7 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
   
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
